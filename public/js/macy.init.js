f16fafcdb740f7baca0a1fc5d1c0f6a253bdede1
Brian A <brianacebo@gmail.com>
2020-04-26 20:22:43 -0400

    Styled project margins

---
+++ public/js/macy.init.js
@@ -0,0 +1,26 @@
+// Find if mobile width or desktop docWidth
+if (window.innerWidth > 768) {
+   var margin = 35;
+} else {
+   var margin = 15;
+}
+
+var masonry = new Macy({
+   container: ".masonryContainer",
+   trueOrder: false,
+   waitForImages: true,
+   useOwnImageLoader: false,
+   debug: true,
+   mobileFirst: false,
+   margin: margin,
+   columns: 4,
+   breakAt: {
+      992: 3,
+      768: 2,
+      595: 2
+   }
+});
+
+masonry.runOnImageLoad(function () {
+   masonry.recalculate(true);
+}, true);
